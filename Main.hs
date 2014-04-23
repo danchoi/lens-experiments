970649dba2bab173a419c41fc7fd06c38957641f
Daniel Choi <dhchoi@gmail.com>
2014-04-23 08:57:26 -0400

    add notes

--- Main.hs
+++ Main.hs
@@ -1,9 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 import qualified Data.ByteString.Lazy.Char8 as BL
 import Data.Aeson
 import Text.Printf
+import Data.Maybe
+
+keys = ["user", "title"]
+
+-- folding function
+f :: Value -> [Value] -> [Value]
+f a b = a : b
 
 main = do
   r <- BL.getContents
-  print $ (decode  r :: Maybe Value)
+  let v = fromJust  $ (decode  r :: Maybe Value)
+  print v
+  let res = foldr f [] v
+  print res
 
