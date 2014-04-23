3b7ed5d51aaf0ff1f8a9a4b31b4c77ede8653995
Daniel Choi <dhchoi@gmail.com>
2014-04-23 10:03:15 -0400

    notes on Value instances defined in Data.Aeson.Lens

--- Main.hs
+++ Main.hs
@@ -5,11 +5,15 @@ import qualified Data.ByteString.Lazy.Char8 as BL
 import Data.Aeson
 import Text.Printf
 import Data.Maybe
+import qualified Data.HashMap.Strict as M
+import qualified Data.Vector as V
 
 keys = ["user", "title"]
 
 -- folding function
 f :: Value -> [Value] -> [Value]
+f (Object o) acc = map f (M.toList o) ++ acc
+f (Array a) acc = map f (V.toList a) ++ acc
 f a b = a : b
 
 main = do
