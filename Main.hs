701d2037d16e2cc012f6749dd485f0d9c030b043
Daniel Choi <dhchoi@gmail.com>
2014-04-23 11:38:15 -0400

    printf with lens json parsing

--- Main.hs
+++ Main.hs
@@ -7,19 +7,28 @@ import Text.Printf
 import Data.Maybe
 import qualified Data.HashMap.Strict as M
 import qualified Data.Vector as V
+import Control.Lens
+import Data.Aeson.Lens
+import Data.Maybe
+import Control.Monad 
+import qualified Data.Text as T
 
-keys = ["user", "title"]
+keys = ["user", "title", "comments"]
 
--- folding function
-f :: Value -> [Value] -> [Value]
-f (Object o) acc = map f (M.toList o) ++ acc
-f (Array a) acc = map f (V.toList a) ++ acc
-f a b = a : b
+item v = 
+  let user = T.unpack  $ v ^. ix "user" . ix "login" . _String
+      title  = T.unpack . fromJust $ v ^? ix "title" ._String
+      comments  = fromJust $ v ^? ix "comments" . _Integer
+      body  = T.unpack . fromJust $ v ^? ix "body" . _String
+  in (user, title, comments, body)
 
 main = do
   r <- BL.getContents
   let v = fromJust  $ (decode  r :: Maybe Value)
-  print v
-  let res = foldr f [] v
-  print res
+  let xs = map item (v ^.. _Array . traverse . _Object)
+  mapM_ (\(u, t, c, _) -> 
+      printf "%-20s %-70s %20d\n" u t c 
+    ) xs
+
+  
 
